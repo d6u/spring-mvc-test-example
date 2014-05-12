550a33297bc19b8460804cff575a127ffe6f67ad
Daiwei <daiweilu123@gmail.com>
2014-05-12 14:59:57 -0400

    Init commit: setup config class and index controller

---
+++ src/test/java/com/company/controller/HelloControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.company.controller;
+
+import com.company.config.AppConfig;
+import com.company.config.WebMvcConfig;
+import com.gargoylesoftware.htmlunit.UnexpectedPage;
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebConnection;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.InputStream;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class, WebMvcConfig.class})
+@WebAppConfiguration
+public class HelloControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private WebClient webClient;
+
+
+    @Before
+    public void setup() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        webClient = new WebClient();
+        webClient.setWebConnection(new MockMvcWebConnection(mockMvc));
+    }
+
+
+    @Test
+    public void testPrintWelcome() throws Exception {
+        UnexpectedPage welcomePage = webClient.getPage("http://localhost/");
+        InputStream is = welcomePage.getInputStream();
+
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        String a = s.hasNext() ? s.next() : "";
+        System.out.println(a);
+    }
+}
