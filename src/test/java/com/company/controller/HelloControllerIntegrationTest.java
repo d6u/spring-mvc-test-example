d528fddb1dd73f8b87f9fdff53b07d73a686bc2f
Daiwei <daiweilu123@gmail.com>
2014-05-14 12:08:23 -0400

    Load @ContextConfiguration with xml files

--- src/test/java/com/company/controller/HelloControllerIntegrationTest.java
+++ src/test/java/com/company/controller/HelloControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.company.controller;
 
-import com.company.config.AppConfig;
-import com.company.config.WebMvcConfig;
 import com.gargoylesoftware.htmlunit.UnexpectedPage;
 import com.gargoylesoftware.htmlunit.WebClient;
 import org.junit.Before;
@@ -19,8 +17,10 @@ import org.springframework.web.context.WebApplicationContext;
 import java.io.InputStream;
 
 
+//@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {AppConfig.class, WebMvcConfig.class})
+//@ContextConfiguration(classes = {AppConfig.class, WebMvcConfig.class})
+@ContextConfiguration(locations = {"file:/Users/daiweilu/dev/_temp/SpringTestPractice/src/main/resources/application-context.xml"})
 @WebAppConfiguration
 public class HelloControllerIntegrationTest {
 
